e9293d3b1e398a28d90e6906bcbf34faba925ea3
unknown <juanjosesierram@gmail.com>
2025-04-09 09:39:15 -0500

    Simple express server with ServiceC dependency

---
+++ index.js
@@ -0,0 +1,14 @@
+import ServiceC from "serviceC";
+import express from 'express';
+
+const app = express();
+
+app.get('/serviceB', (req, res) => {
+    res.send('Log from ServiceB');
+});
+
+app.get('/serviceC', (req, res) => {
+    res.send(ServiceC());
+});
+
+export default app;
--- server.js
+++ server.js
@@ -1,12 +1,6 @@
+import app from './index.js';
 
-const express = require('express');
-const app = express();
 const port = 3000;
-
-app.get('/', (req, res) => {
-  res.send('hello world');
-});
-
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
